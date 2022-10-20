89c1c9534b09e968e818a4ad29fa625fd5dd55da
Ashley-byte <116314975+Ashley-byte@users.noreply.github.com>
2022-10-20 22:42:16 +0100

    Add files via upload

---
+++ Ash.cpp
@@ -0,0 +1,24 @@
+#include<studio.h>
+extern int a, b, c, d, e;
+extern int f;
+extern float g;
+int main()
+{
+    // variable defination
+    int a, b, c, d, e;
+    int f;
+    float g;
+    // variable innitialisation
+    a=10
+    b=12
+    c=16
+    d=8
+    e=4
+    f= a+b+c+d+e
+    prinf("value of f;%f\n",f);
+    
+    g=f/5
+    printf("Average of g; %g\n"; g);
+    
+    return 0;
+}
